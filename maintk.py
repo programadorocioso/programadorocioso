d64bf39002c115e6e91023471da850f1d51f4865
programadorocioso <57318225+programadorocioso@users.noreply.github.com>
2022-10-27 02:09:33 -0300

    Add files via upload

--- maintk.py
+++ maintk.py
@@ -4,10 +4,10 @@ from verificar_cotacao import cotacao
 from modulo_arquivo_tk import leitura_escrita_excell, xlsx_para_pdf
 from datetime import datetime
 from functools import partial
-import base64
-from icone import imagem
-from os import remove
-imagem_icone = base64.b64decode(imagem())
+#import base64
+#from icone import imagem
+#from os import remove
+#imagem_icone = base64.b64decode(imagem())
 #Remover comentários das linhas abaixo se executando no Windows
 #Por algum motivo muito específico, essas linhas causam erros apenas no Linux
 #Mais especificadamente a linha contendo o método "iconbitmap"
@@ -24,16 +24,21 @@ def main_func(ativos):
         dados_planilha.append([ativo.upper(),cotacao(ativo),data_hora])
     leitura_escrita_excell(dados_planilha)
     xlsx_para_pdf()
+    campo_operacao = tk.Label(principal,text="Pronto",font="Arial 14",padx=10,pady=10)
+    campo_operacao.pack()
 principal = tk.Tk()
 #principal.iconbitmap(tmpfl)
 #remove(tmpfl)
-principal.geometry('350x100')
+principal.geometry('500x200')
 principal.title("StonksWare")
 var = StringVar(principal)
 entrada = var.get()
 texto = "Códigos do Google Finance (separados por espaço)"
 nome_botao = "Buscar cotações"
-nome_campo = tk.Label(principal,text=texto).grid(row=0,column=0)
-campo = tk.Entry(principal,textvariable=var).grid(row=1,column=0)
-botao = tk.Button(principal,text=nome_botao,command=partial(main_func,entrada)).grid(row=2,column=0)
+nome_campo = tk.Label(principal,text=texto,font="Arial 14",padx=10,pady=10)
+campo = tk.Entry(principal,textvariable=var,font="Arial 14")
+botao = tk.Button(principal,text=nome_botao,command=partial(main_func,entrada),padx=10,pady=10,font="Arial 12")
+nome_campo.pack()
+campo.pack()
+botao.pack()
 principal.mainloop()
